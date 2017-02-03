2e27e919e0b6716eeb6983e9359f0d5255b31d62
Michael R. Plews <michael.plews@gmail.com>
2017-02-03 10:12:31 -0600

    Fixed bugs in status_return

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ git clone git@github.com:CabanaLab/LabPi.git
 
 ### `empty.py`
 Setup `empty.py` to run on startup in a headless commandline environment (no GUI). This script is designed to await input from a barcode scanner on a forever-loop. 
+
+### `lcs_16x2.py`
+Prints output to a [16x2 character LCD screen](https://www.adafruit.com/products/198). This item has been modified for the purposes here. [The original creator and file can be found here.](http://www.raspberrypi-spy.co.uk/)
--- empty.py
+++ empty.py
@@ -1,11 +1,10 @@
 """Used in conjunction with https://github.com/m3wolf/professor_oak
 (Professor Oak Lab Management System). Script to run on a Raspberry Pi
 that will send a HTTPrequest to the main inventory server to mark a
-certain container_id as empty and hopefully, if it's not too
-complicated to program, output a success message to a GPIO LCD
+certain container_id as empty and output a success/failure message to a GPIO LCD
 display. Questions? Comments? email michael.plews@gmail.com"""
 
-import re, datetime, requests, json, LOCALSETTINGS as localsettings
+import re, datetime, requests, json, LOCALSETTINGS as localsettings, lcd_16x2 as lcd
 
 import logging
 
@@ -74,23 +73,61 @@ def mark_as_empty(id_number):
     log.debug("Received response text: %s", r.text)
 
     r = requests.patch(url, json=payload, auth=auth)
-  
+    return r.status_code  
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
+    lcd.lcd_init()
+    lcd.lcd_string("Welcome", lcd.LCD_LINE_1, "c")
+    lcd.lcd_string("to LabPi", lcd.LCD_LINE_2, "c")
+    lcd.GPIO.cleanup()
+    lcd.time.sleep(3)
     while True:
-        barcode = str(input('input:'))
-        log.debug("Input received: %s", barcode)
-        if input == 'EXIT':
-            log.info("Received 'EXIT' signal. Exiting.")
-            exit()
-        if validate(barcode):
-            if barcode[:2] == 'UL':
-                status_return = send_notification(barcode[2:], note_type='ULON')
-                barcode_string = str(barcode)
-                log.info("ULON# %s %s", str(barcode).zfill(barcode_digit_length), status_return)
+        try:
+            lcd.GPIO.cleanup()
+            lcd.lcd_init()
+            lcd.lcd_string("Ready:", lcd.LCD_LINE_1)
+            barcode = str(input('input:'))
+            log.debug("Input received: %s", barcode)
+            if barcode == 'EXIT':
+                log.info("Received 'EXIT' signal. Exiting.")
+                exit()
+            if validate(barcode):
+                lcd.lcd_string("Recieved:", lcd.LCD_LINE_1)
+                lcd.lcd_string(barcode, lcd.LCD_LINE_2)
+                if barcode[:2] == 'UL':
+                    lcd.lcd_string("Sending...", lcd.LCD_LINE_1)
+                    status_return = send_notification(barcode[2:], note_type='ULON')
+                    barcode_string = str(barcode)
+                    log.info("ULON# %s %s", str(barcode).zfill(barcode_digit_length), status_return)
+                    if status_return == "200":
+                        lcd.lcd_string("Email Sent!", lcd.LCD_LINE_1)
+                        lcd.GPIO.cleanup()
+                        lcd.time.sleep(2)
+                    else:
+                        lcd.lcd_string("Error!", lcd.LCD_LINE_1)
+                        lcd.GPIO.cleanup()
+                        lcd.time.sleep(2)
+                else:
+                    lcd.lcd_string("Sending...", lcd.LCD_LINE_1)
+                    status_return = mark_as_empty(barcode)
+                    barcode_string = str(barcode).zfill(barcode_digit_length)
+                    log.info("ID# %s %s", str(barcode).zfill(barcode_digit_length), status_return)
+                    if status_return == 200:
+                        lcd.lcd_string("Marked as Empty!", lcd.LCD_LINE_1)
+                        lcd.GPIO.cleanup()
+                        lcd.time.sleep(2)
+                    else:
+                        lcd.lcd_string("Error!", lcd.LCD_LINE_1)
+                        lcd.GPIO.cleanup()
+                        lcd.time.sleep(2)                
             else:
-                status_return = mark_as_empty(barcode)
-                barcode_string = str(barcode).zfill(barcode_digit_length)
-                log.info("ID# %s %s", str(barcode).zfill(barcode_digit_length), status_return)
-        else:
-            log.warning("Invalid input: %s", barcode)
+                log.warning("Invalid input: %s", barcode)
+                lcd.lcd_string("Invalid", lcd.LCD_LINE_1)
+                lcd.GPIO.cleanup()
+                lcd.time.sleep(2)
+        except (KeyboardInterrupt, SystemExit):
+            lcd.lcd_string("Goodbye!",lcd.LCD_LINE_1, "c")
+            lcd.time.sleep(1)
+            lcd.GPIO.cleanup()
+            lcd.lcd_init()
+            exit()
